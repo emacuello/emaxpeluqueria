4b57bc3f1c52f134fbf90b0e577568604b6266c3
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-07-03 19:17:24 -0300

    fix redis

--- back/src/config/redis.config.ts
+++ back/src/config/redis.config.ts
@@ -1,8 +1,8 @@
-import { Redis } from 'ioredis';
-import { HOST_REDIS, PASSWORD_REDIS, PORT_REDIS } from './envs';
+// import { Redis } from 'ioredis';
+// import { HOST_REDIS, PASSWORD_REDIS, PORT_REDIS } from './envs';
 
-export const subscriber = new Redis({
-	host: HOST_REDIS,
-	port: Number(PORT_REDIS),
-	password: PASSWORD_REDIS,
-});
+// export const subscriber = new Redis({
+// 	host: HOST_REDIS,
+// 	port: Number(PORT_REDIS),
+// 	password: PASSWORD_REDIS,
+// });
